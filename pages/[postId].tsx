70fddbb1f8fe618b0251f0b4755164ccd3a47488
Ngô Vưn Nam <anh02284@gmail.com>
2022-12-21 13:49:07 +0700

    feat: fallback

--- pages/[postId].tsx
+++ pages/[postId].tsx
@@ -1,47 +1,52 @@
-import React, { Fragment } from 'react'
+import React, { Fragment } from 'react';
 
-type Props = {}
+type Props = {};
 
 const PostDetails = (props: any) => {
-    const { post } = props
-    return (
-        <Fragment><div>{post.id}</div>
-            <div>{post.title}</div>
-            <div>{post.body}</div>
-        </Fragment>
+	const { post } = props;
 
-    )
-}
+	if (!post) return <div>Loading post.....</div>; // fallback : true
+	return (
+		<Fragment>
+			<div>{post.id}</div>
+			<div>{post.title}</div>
+			<div>{post.body}</div>
+		</Fragment>
+	);
+};
 
 export async function getStaticProps(context: any) {
-    console.log(1111111, context);
-    const { params } = context;
-    const postId = +params.postId;
-    if (!postId) {
-        return {
-            notFound: true
-        }
-    }
-    const response = await fetch('http://jsonplaceholder.typicode.com/posts');
-    const posts = await response.json();
-    console.log('postId', postId);
+	console.log(1111111, context);
+	const { params } = context;
+	const postId = +params.postId;
+	if (!postId) {
+		return {
+			notFound: true,
+		};
+	}
+	const response = await fetch('http://jsonplaceholder.typicode.com/posts');
+	const posts = await response.json();
+	console.log('postId', postId);
 
-    const post = posts.find((post: any) => post.id === postId);
-    return {
-        props: {
-            post
-        }
-    }
+	const post = posts.find((post: any) => post.id === postId);
+	return {
+		props: {
+			post,
+		},
+	};
 }
 
 export async function getStaticPaths() {
-    const response = await fetch('http://jsonplaceholder.typicode.com/posts');
-    const posts = await response.json();
-    const params = posts.map((post: any) => ({ params: { postId: post.id.toString() } }))
-    return {
-        paths: params,
-        fallback: false
-    }
+	const response = await fetch('http://jsonplaceholder.typicode.com/posts');
+	const posts = await response.json();
+	const params = posts.map((post: any) => ({ params: { postId: post.id.toString() } }));
+	return {
+		paths: params,
+		fallback: false,
+        //if fallback : fasle thi nhung path kh tra ve ở params khi truy cập vào sẽ chạy vào 404 page
+		// if fallback : true khi những params kh được pre rendering phải xử check empty truócw khi render
+		//if fallback :'blocking' có thể kh cần check empty vì sẽ được pre rendering trên server đợi server load xong => return về detail pages
+	};
 }
 
-export default PostDetails
+export default PostDetails;
